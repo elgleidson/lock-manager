288bcd39662d283c292caaf6be28efdf1f9f33c7
Gleidson Leopoldo <el.gleidson@gmail.com>
2024-09-01 20:28:47 +0100

    simplest in memory implementation

--- lock-manager/src/main/java/com/github/elgleidson/lock/LockManagerInMemory.java
+++ lock-manager/src/main/java/com/github/elgleidson/lock/LockManagerInMemory.java
@@ -3,25 +3,25 @@ package com.github.elgleidson.lock;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.ZonedDateTime;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Slf4j
-public class LockManagerRedis implements LockManager {
+public class LockManagerInMemory implements LockManager {
 
-  protected static final String KEYSPACE = "lock:";
-
-  private final StringRedisTemplate stringRedisTemplate;
+  private final Map<String, String> locks = new ConcurrentHashMap<>();
   private final Clock clock;
   private final UUIDWrapper uuidWrapper;
 
-  public LockManagerRedis(StringRedisTemplate stringRedisTemplate) {
-    this(stringRedisTemplate, Clock.systemUTC(), new UUIDWrapper());
+  public LockManagerInMemory() {
+    this(Clock.systemUTC(), new UUIDWrapper());
   }
 
-  protected LockManagerRedis(StringRedisTemplate stringRedisTemplate, Clock clock, UUIDWrapper uuidWrapper) {
-    this.stringRedisTemplate = stringRedisTemplate;
+  public LockManagerInMemory(Clock clock, UUIDWrapper uuidWrapper) {
+    log.warn("*** THIS LOCK MANAGER ONLY WORKS FOR A SINGLE INSTANCE! ***");
+    log.warn("Consider using another implementation!");
     this.clock = clock;
     this.uuidWrapper = uuidWrapper;
   }
@@ -29,18 +29,18 @@ public class LockManagerRedis implements LockManager {
   @Override
   public Lock lock(String uniqueIdentifier, Duration expiresIn) {
     try {
+      log.debug("trying to acquire lock for {}, expiring in {}", uniqueIdentifier, expiresIn);
       var lock = createLock(uniqueIdentifier, expiresIn);
-      var inserted = stringRedisTemplate.opsForValue().setIfAbsent(lockKey(uniqueIdentifier), lock.id(), expiresIn);
-      if (!Boolean.TRUE.equals(inserted)) {
-        // this is to track concurrent calls
+      var id = locks.putIfAbsent(uniqueIdentifier, lock.id());
+      if (id != null) {
         log.warn("error lock(): lock already acquired on '{}'!", uniqueIdentifier);
         throw LockFailureException.alreadyLocked(uniqueIdentifier);
       }
+      log.debug("locked={}", lock);
       return lock;
+    } catch (LockFailureException lockFailureException) {
+      throw lockFailureException;
     } catch (Exception ex) {
-      if (ex instanceof LockFailureException) {
-        throw ex;
-      }
       log.error("error lock(): message={}", ex.getMessage());
       throw LockFailureException.other(uniqueIdentifier, ex);
     }
@@ -49,19 +49,17 @@ public class LockManagerRedis implements LockManager {
   @Override
   public boolean unlock(Lock lock) {
     try {
+      log.debug("trying to unlock {}", lock);
       // only unlocks if the lock id matches as uniqueIdentifier is the cache key
-      var lockKey = lockKey(lock.uniqueIdentifier());
-      var value = stringRedisTemplate.opsForValue().get(lockKey);
       // if the value is different it means either the lock has already expired or it was released and other process has acquired the lock on the same unique identifier
       // in this case, does not unlock it as it needs to be unlocked by the process that has acquired the lock, or it will expire automatically
       // log it for tracking purposes!
-      if (!lock.id().equals(value)) {
+      var unlocked = locks.remove(lock.uniqueIdentifier(), lock.id());
+      if (!unlocked && locks.containsKey(lock.uniqueIdentifier())) {
         log.warn("unlock(): another process has acquired the lock on '{}'", lock.uniqueIdentifier());
-        return false;
       }
-      var unlocked = stringRedisTemplate.delete(lockKey);
       log.debug("unlocked={}", unlocked);
-      return Boolean.TRUE.equals(unlocked);
+      return unlocked;
     } catch (Exception ex) {
       // log the error, but returns successfully as the lock will expire (TTL)
       log.error("error unlock(): message={}", ex.getMessage());
@@ -75,15 +73,10 @@ public class LockManagerRedis implements LockManager {
     return new Lock(id, uniqueIdentifier, expiresAt);
   }
 
-  private String lockKey(String uniqueIdentifier) {
-    return KEYSPACE + uniqueIdentifier;
-  }
-
   protected static class UUIDWrapper {
 
     protected UUID randomUUID() {
       return UUID.randomUUID();
     }
   }
-
 }
---
+++ lock-manager/src/test/java/com/github/elgleidson/lock/cucumber/CucumberSteps.java
@@ -0,0 +1,57 @@
+package com.github.elgleidson.lock.cucumber;
+
+import com.github.elgleidson.lock.LockManager;
+import com.github.elgleidson.lock.TestApplication;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = TestApplication.class, webEnvironment = WebEnvironment.NONE)
+@Slf4j
+public class CucumberSteps extends CucumberStepsBase {
+
+  public CucumberSteps(LockManager lockManager) {
+    super(lockManager);
+  }
+
+  @Before
+  public void before() {
+    super.before();
+  }
+
+  @Given("an existing record with id of {string}")
+  public void givenAnExistingRecordWithIdOf(String id) {
+    super.givenAnExistingRecordWithIdOf(id);
+  }
+
+  @When("I call the update {int} time(s) concurrently with id {string}")
+  public void callTheUpdateConcurrently(int concurrency, String id) {
+    super.callTheUpdateConcurrently(concurrency, id);
+  }
+
+  @When("I call the update {int} time(s) sequentially with id {string}")
+  public void callTheUpdateSequentially(int times, String id) {
+    super.callTheUpdateSequentially(times, id);
+  }
+
+  @When("I call the lock update {int} time(s) concurrently with id {string}")
+  public void callTheLockUpdateConcurrently(int concurrency, String id) {
+    super.callTheLockUpdateConcurrently(concurrency, id);
+  }
+
+  @When("I call the lock update {int} time(s) sequentially with id {string}")
+  public void callTheLockUpdateSequentially(int times, String id) {
+    super.callTheLockUpdateSequentially(times, id);
+  }
+
+  @Then("the record with id {string} is updated {int} time(s)")
+  public void thenTheRecordIsUpdated(String id, int expectedUpdates) {
+    super.thenTheRecordIsUpdated(id, expectedUpdates);
+  }
+}
