0f7e039441d081b83502ac61a9b7f3b66a19e374
Gleidson Leopoldo <el.gleidson@gmail.com>
2024-09-03 19:32:07 +0100

    test-jar removed for simplicity

--- lock-manager-reactive-mongodb/src/test/java/com/github/elgleidson/lock/cucumber/CucumberSteps.java
+++ lock-manager-reactive-mongodb/src/test/java/com/github/elgleidson/lock/cucumber/CucumberSteps.java
@@ -1,5 +1,8 @@
 package com.github.elgleidson.lock.cucumber;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.elgleidson.lock.LockFailureException;
 import com.github.elgleidson.lock.ReactiveLockManager;
 import com.github.elgleidson.lock.TestApplication;
 import io.cucumber.java.Before;
@@ -9,9 +12,19 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiFunction;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = TestApplication.class, webEnvironment = WebEnvironment.NONE, properties = {
@@ -19,69 +32,110 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
   "spring.data.mongodb.auto-index-creation=true"
 })
 @Slf4j
-public class CucumberSteps extends CucumberStepsBase {
+public class CucumberSteps {
 
-  public CucumberSteps(ReactiveLockManager lockManager) {
-    super(lockManager);
-  }
+  private final Map<String, AtomicInteger> db = new ConcurrentHashMap<>();
 
-  @Override
+  @Autowired
+  private ReactiveLockManager lockManager;
+
+  private Duration ttl;
+  private Duration delay;
+
+  private Flux<Boolean> responses;
+
   @Before
   public void before() {
-    super.before();
+    db.clear();
   }
 
-  @Override
-  @ParameterType(DURATION_REGEX)
+  @ParameterType("(\\d+(s|ms))")
   public Duration duration(String duration) {
-    return super.duration(duration);
+    var amount = duration.replaceFirst("(ms|s)", "");
+    var timeUnit = duration.replaceFirst(amount, "");
+    return timeUnit.equals("ms")
+      ? Duration.ofMillis(Long.parseLong(amount))
+      : Duration.ofSeconds(Long.parseLong(amount));
   }
 
-  @Override
   @Given("the lock expires in {duration}")
   public void givenLockExpiresIn(Duration duration) {
-    super.givenLockExpiresIn(duration);
+    this.ttl = duration;
   }
 
-  @Override
   @Given("the process takes {duration}")
   public void givenTheProcessTakes(Duration duration) {
-    super.givenTheProcessTakes(duration);
+    this.delay = duration;
   }
 
-  @Override
   @Given("an existing record with id of {string}")
   public void givenAnExistingRecordWithIdOf(String id) {
-    super.givenAnExistingRecordWithIdOf(id);
+    db.put(id, new AtomicInteger(0));
   }
 
-  @Override
   @When("I call the update {int} time(s) concurrently with id {string}")
   public void callTheUpdateConcurrently(int concurrency, String id) {
-    super.callTheUpdateConcurrently(concurrency, id);
+    callUpdateConcurrently(concurrency, id, this::update);
   }
 
-  @Override
   @When("I call the update {int} time(s) sequentially with id {string}")
   public void callTheUpdateSequentially(int times, String id) {
-    super.callTheUpdateSequentially(times, id);
+    callUpdateSequentially(times, id, this::update);
   }
 
-  @Override
   @When("I call the lock update {int} time(s) concurrently with id {string}")
   public void callTheLockUpdateConcurrently(int concurrency, String id) {
-    super.callTheLockUpdateConcurrently(concurrency, id);
+    callUpdateConcurrently(concurrency, id, this::updateLock);
   }
 
-  @Override
   @When("I call the lock update {int} time(s) sequentially with id {string}")
   public void callTheLockUpdateSequentially(int times, String id) {
-    super.callTheLockUpdateSequentially(times, id);
+    callUpdateSequentially(times, id, this::updateLock);
   }
 
-  @Override
+  private void callUpdateSequentially(int times, String id, BiFunction<Integer, String, Mono<Boolean>> function) {
+    responses = Flux.range(1, times)
+      .concatMap(i -> function.apply(i, id)
+        .doFirst(() -> log.info("sequential exec={}: start", i))
+        .doOnSuccess(unused -> log.info("sequential exec={}: end", i))
+      )
+      .subscribeOn(Schedulers.single());
+  }
+
+  @SneakyThrows
+  private void callUpdateConcurrently(int concurrency, String id, BiFunction<Integer, String, Mono<Boolean>> function) {
+    responses = Flux.range(1, concurrency)
+      .parallel(concurrency)
+      .runOn(Schedulers.newParallel("parallel", concurrency))
+      .flatMap(i -> function.apply(i, id)
+        .doFirst(() -> log.info("parallel exec={}: start", i))
+        .doOnSuccess(unused -> log.info("parallel exec={}: end", i))
+      )
+      .sequential()
+      .subscribeOn(Schedulers.single());
+  }
+
+  private Mono<Boolean> update(int exec, String id) {
+    return Mono.just(db.get(id))
+      .delayElement(delay) // to simulate processing
+      .map(AtomicInteger::incrementAndGet)
+      .doOnNext(updates -> log.info("exec={}: updated id={}, updates={}", exec, id, updates))
+      .thenReturn(true)
+      .doFirst(() -> log.info("exec={}: updating id={}", exec, id));
+  }
+
+  private Mono<Boolean> updateLock(int exec, String id) {
+    return lockManager.wrap(id, ttl, true, () -> update(exec, id))
+      .onErrorResume(LockFailureException.class, e -> {
+        log.error("exec={}: id={}, locked", exec, id);
+        return Mono.just(false);
+      });
+  }
+
   @Then("the record with id {string} is updated {int} time(s)")
   public void thenTheRecordIsUpdated(String id, int expectedUpdates) {
-    super.thenTheRecordIsUpdated(id, expectedUpdates);
+    StepVerifier.create(responses.then()).verifyComplete();
+    var updates = db.get(id).get();
+    assertThat(updates).isEqualTo(expectedUpdates);
   }
 }
--- lock-manager-redis/src/test/java/com/github/elgleidson/lock/cucumber/CucumberSteps.java
+++ lock-manager-redis/src/test/java/com/github/elgleidson/lock/cucumber/CucumberSteps.java
@@ -1,5 +1,8 @@
 package com.github.elgleidson.lock.cucumber;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.elgleidson.lock.LockFailureException;
 import com.github.elgleidson.lock.LockManager;
 import com.github.elgleidson.lock.TestApplication;
 import io.cucumber.java.AfterAll;
@@ -11,7 +14,14 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiFunction;
+import java.util.stream.IntStream;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import redis.embedded.RedisServer;
@@ -19,9 +29,17 @@ import redis.embedded.RedisServer;
 @CucumberContextConfiguration
 @SpringBootTest(classes = TestApplication.class, webEnvironment = WebEnvironment.NONE)
 @Slf4j
-public class CucumberSteps extends CucumberStepsBase {
+public class CucumberSteps {
 
   private static final RedisServer redisServer = RedisServer.builder().port(6379).build();
+
+  private final Map<String, AtomicInteger> db = new ConcurrentHashMap<>();
+
+  @Autowired
+  private LockManager lockManager;
+
+  private Duration ttl;
+  private Duration delay;
 
   @BeforeAll
   public static void beforeAll()  {
@@ -33,67 +51,92 @@ public class CucumberSteps extends CucumberStepsBase {
     redisServer.stop();
   }
 
-  public CucumberSteps(LockManager lockManager) {
-    super(lockManager);
-  }
-
-  @Override
   @Before
   public void before() {
-    super.before();
+    db.clear();
   }
 
-  @Override
-  @ParameterType(DURATION_REGEX)
+  @ParameterType("(\\d+(s|ms))")
   public Duration duration(String duration) {
-    return super.duration(duration);
+    var amount = duration.replaceFirst("(ms|s)", "");
+    var timeUnit = duration.replaceFirst(amount, "");
+    return timeUnit.equals("ms")
+      ? Duration.ofMillis(Long.parseLong(amount))
+      : Duration.ofSeconds(Long.parseLong(amount));
   }
 
-  @Override
   @Given("the lock expires in {duration}")
   public void givenLockExpiresIn(Duration duration) {
-    super.givenLockExpiresIn(duration);
+    this.ttl = duration;
   }
 
-  @Override
   @Given("the process takes {duration}")
   public void givenTheProcessTakes(Duration duration) {
-    super.givenTheProcessTakes(duration);
+    this.delay = duration;
   }
 
-  @Override
   @Given("an existing record with id of {string}")
   public void givenAnExistingRecordWithIdOf(String id) {
-    super.givenAnExistingRecordWithIdOf(id);
+    db.put(id, new AtomicInteger(0));
   }
 
-  @Override
   @When("I call the update {int} time(s) concurrently with id {string}")
   public void callTheUpdateConcurrently(int concurrency, String id) {
-    super.callTheUpdateConcurrently(concurrency, id);
+    callUpdateConcurrently(concurrency, id, this::update);
   }
 
-  @Override
   @When("I call the update {int} time(s) sequentially with id {string}")
   public void callTheUpdateSequentially(int times, String id) {
-    super.callTheUpdateSequentially(times, id);
+    callUpdateSequentially(times, id, this::update);
   }
 
-  @Override
   @When("I call the lock update {int} time(s) concurrently with id {string}")
   public void callTheLockUpdateConcurrently(int concurrency, String id) {
-    super.callTheLockUpdateConcurrently(concurrency, id);
+    callUpdateConcurrently(concurrency, id, this::updateLock);
   }
 
-  @Override
   @When("I call the lock update {int} time(s) sequentially with id {string}")
   public void callTheLockUpdateSequentially(int times, String id) {
-    super.callTheLockUpdateSequentially(times, id);
+    callUpdateSequentially(times, id, this::updateLock);
   }
 
-  @Override
+  private void callUpdateSequentially(int times, String id, BiFunction<Integer, String, Boolean> function) {
+    IntStream.range(1, times+1).boxed().sequential().forEach(i -> {
+      log.info("sequential exec={}: start", i);
+      function.apply(i, id);
+      log.info("sequential exec={}: end", i);
+    });
+  }
+
+  private void callUpdateConcurrently(int concurrency, String id, BiFunction<Integer, String, Boolean> function) {
+    IntStream.range(1, concurrency+1).boxed().parallel().forEach(i -> {
+      log.info("parallel exec={}: start", i);
+      function.apply(i, id);
+      log.info("parallel exec={}: end", i);
+    });
+  }
+
+  @SneakyThrows
+  private boolean update(int exec, String id) {
+    log.info("exec={}: updating id={}", exec, id);
+    Thread.sleep(delay); // to simulate processing
+    var updates = db.get(id).incrementAndGet();
+    log.info("exec={}: updated id={}, updates={}", exec, id, updates);
+    return true;
+  }
+
+  private boolean updateLock(int exec, String id) {
+    try {
+      return lockManager.wrap(id, ttl, () -> update(exec, id));
+    } catch (LockFailureException e) {
+      log.error("exec={}: id={}, locked", exec, id);
+      return false;
+    }
+  }
+
   @Then("the record with id {string} is updated {int} time(s)")
   public void thenTheRecordIsUpdated(String id, int expectedUpdates) {
-    super.thenTheRecordIsUpdated(id, expectedUpdates);
+    var updates = db.get(id).get();
+    assertThat(updates).isEqualTo(expectedUpdates);
   }
 }
